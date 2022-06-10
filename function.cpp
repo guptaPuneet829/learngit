c02933d8b851a6017f329ad588272ff94a7787eb
Puneet Gupta <pg0058007@gmail.com>
2022-06-10 11:01:52 +0530

    function.cpp file added successfully

---
+++ function.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int add(int a,int b){
+	return a+b;
+}
+
+int sub(int a,int b){
+	return a-b;
+}
+
+int mul(int a,int b){
+	return a*b;
+}
+
+
+int main(){
+	cout<<"Addition,Subtraction and Multiplication of two numbers 6,3 are respectively : "<<add(6,3)<<","<<sub(6,3)<<","<<mul(6,3)<<endl;
+	return 0;
+}
